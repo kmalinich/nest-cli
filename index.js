d13f6daa8c15f7f1901cd99d9b58eedea379d2c6
kdm <kennygprs@gmail.com>
2017-09-11 14:43:51 -0400

    Update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const CLI = require('./lib/cli');
 const UI = require('./lib/ui');
 
 const ui = new UI({
-  inputStream: process.stdin,
-  outputStream: process.stdout
+	inputStream  : process.stdin,
+	outputStream : process.stdout,
 });
 
 const app = new App(ui, pkg.version, process.env.NODE_ENV);
@@ -16,5 +16,5 @@ const cli = new CLI(app, ui);
 cli.parse(process.argv);
 
 if (!cli.args().length) {
-  cli.help();
+	cli.help();
 }
