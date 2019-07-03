e1ae4dd915ec3eb34cb2ef8d681707c1bc424ce1
kdm <kennygprs@gmail.com>
2019-07-03 16:46:09 -0400

    Updated dependencies

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-'use strict';
-
 const pkg = require('./package.json');
 const App = require('./lib/app');
 const CLI = require('./lib/cli');
-const UI = require('./lib/ui');
-
-const ui = new UI({
-	inputStream  : process.stdin,
-	outputStream : process.stdout,
-});
+const ui  = require('./lib/ui');
 
 const app = new App(ui, pkg.version, process.env.NODE_ENV);
 const cli = new CLI(app, ui);
